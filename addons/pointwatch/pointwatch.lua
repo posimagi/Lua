8c185f399cefadbb217e9892b46e3495f89a7a63
Posimagi <posimagi@gmail.com>
2022-03-22 00:37:31 -0700

    [pointwatch] add job names and abbreviations

--- addons/pointwatch/pointwatch.lua
+++ addons/pointwatch/pointwatch.lua
@@ -112,9 +112,12 @@ packet_handlers = {
     end,
     [0x061] = function(org)
         local p = packets.parse('incoming',org)
+        ]
         xp.current = p['Current EXP']
         xp.tnl = p['Required EXP']
+        xp.job = p['Main Job']
         xp.job_level = p['Main Job Level']
+        xp.sub_job = p['Sub Job']
         xp.sub_job_level = p['Sub Job Level']
         accolades.current = p['Unity Points']
         ep.current = p['Current Exemplar Points']
